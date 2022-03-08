ed1387efdfd9dbbdbaca83f214c3380243ff36de
mhbauleo <mhbfiuba@gmail.com>
2022-03-08 03:58:57 -0300

    dirname

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 
 const PORT = 8080
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
 
 const server = app.listen(PORT, () => {
     console.log(`servidor levantado en el puerto ${server.address().port}`)
